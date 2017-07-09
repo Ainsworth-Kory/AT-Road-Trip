646cf868fa1e2252a7593c142c3342a3d73fa790
IanandJenna <IanandJenna@LAPTOP-778NNG5G>
2017-07-08 20:42:44 -0700

    7/8/17 Added lesson 12 team assignment to project attempted to complete individual assignment

--- src/byui/cit260/atRoadTrip1/view/TripInfoView.java
+++ src/byui/cit260/atRoadTrip1/view/TripInfoView.java
@@ -6,6 +6,10 @@
 package byui.cit260.atRoadTrip1.view;
 
 import byui.cit260.atRoadTrip1.control.CarControl;
+import byui.cit260.atRoadTrip1.model.Item;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 /**
  *
@@ -29,34 +33,58 @@ public class TripInfoView extends View{
         
         
     }
-
-    public static TripInfoView getGameReport() {
-        System.out.println("\n"
-                  + "\n--------------------------------------"
-                  + "\n           List of Trip Info                 "
-                  + "\n--------------------------------------"
-                  + "\nV - View Trip Info"
-                  + "\nD - Drive"
-                  + "\nC - Count Cash"
-                  + "\nG - Stop for gas"
-                  + "\nF - Stop for food"
-                  + "\nP - Stop for food & gas"
-                  + "\nM - Stop for motel"
-                  + "\nS - Save Game"
-                  + "\nH - Help Menu"
-                  + "\nR - Print Game Report"
-                  + "\nQ - Quit"
-                  + "\n--------------------------------------");
-   
+// This is not the correct way to do this should be char stream
+    static TripInfoView getGameReport() {
+        System.out.println("\n        Inventory Items"
+                             + "\nDescription      Quantity"
+                              + " \n Car              One"
+                                + "\nCash             $500"
+                               + "\n Time             30 hours"
+                               + "\n Distance         2000 miles"); //To change body of generated methods, choose Tools | Templates.
             return null;
+    }
+// This is my attempt at char stream but would not work
+  /*  public static TripInfoView getGameReport(String outputLocation) {
+       try (PrintWriter out = new PrintWriter(outputLocation)) {
+       
+              out.println("\n\n     Game Report               ");
+               out.printf("%n%-20s%10s%10s", "List of", "Inventory", "Items");
+                out.printf("%n%-20s%10s%10s", "Gas", "Cash", "Time", "Distance");
+                
+       } catch (Exception e){
+           ErrorView.display("I/O Error: ");
+       }
+            return null;
+           
+    }*/
         
          //To change body of generated methods, choose Tools | Templates.
-    }
-
+/*
+    static TripInfoView getGameReport(String outputLocation) {
+            
+       try (PrintWriter out = new PrintWriter(outputLocation)) {
+              // print title and column headings
+              out.println("\n\n     List of Inventory Items      ");
+               out.printf("%n%-20s%10s", "Description", "Quantity");
+                out.printf("%n%-20s%10s", "----------", "--------");
+          for (Item item : inventoryItem){
+              out.printf("%n%-20s%7d", item.getDescripton()
+                                      , item.getQuntity());
+          }      
+       } catch (Exception e){
+           ErrorView.display("I/O Error: ");
+       }
+            return null;
+           
+    }*/
+    
     @Override
     public boolean doAction(String value) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  
     }
-    
-    
+
 }
+    
+    
+
